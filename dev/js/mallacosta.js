7588405e2928d097c9427600919f1d55207d68a7
David Chay <davichay@gmail.com>
2017-02-09 08:18:01 -0600

    diseño avanzado

--- dev/js/mallacosta.js
+++ dev/js/mallacosta.js
@@ -1,7 +1,3 @@
-$(function () {
-
-});
-
 $( window ).resize(function() {
   if($(window).width()<768){
     $('#navigation').addClass('nav-justified');
@@ -10,6 +6,33 @@ $( window ).resize(function() {
     $('#navigation').removeClass('nav-justified');
     $('#navigation li > a > span ').removeClass('smlts');
   }
-  console.log($(window).width());
-  //$( "body" ).prepend( "<div>" + $( window ).width() + "</div>" );
+});
+
+$(function () {
+  ///Mustache template
+
+  $.getJSON('../data/productos.json', function(data) {
+    console.log(data);
+    var prd={};
+    prd.productos=data;
+    //productos card.
+    var plantilla=$('#productCardTlp').html();
+    var template=Handlebars.compile(plantilla);
+    var html=template(prd);
+    $("#prodcutosCard .row").append(html);
+    $("#prodcutosCard .row").show();
+
+    //productos Modal
+    var pltModal=$('#productModalTlp').html();
+    var templateModal=Handlebars.compile(pltModal);
+    var htmlModal=templateModal(prd);
+    $("#modalArea").append(htmlModal);
+  //  $("#modalAreatt").show();
+  });
+  // $.getJSON('../data/productos.json', function(data) {
+  //   var templateProductos = $('#productCardTlp').html();
+  //   var htmlProductos = Mustache.to_html(templateProductos, data);
+  //  #prodcutosCard').html(htmlProductos);
+  // });
+
 });
